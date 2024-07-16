3cc466361974e9d4e155cecd57faa3ffcc6b2119
fam-rizz-ve <fam.rizz.ve@gmail.com>
2024-07-16 08:29:31 +0200

    spegnimento

---
+++ spegnimento.bat
@@ -0,0 +1,24 @@
+@ECHO OFF
+color 40
+echo il computer si spegnerà tra:
+echo 10
+timeout /t 1 /nobreak
+echo 9
+timeout /t 1 /nobreak
+echo 8
+timeout /t 1 /nobreak
+echo 7
+timeout /t 1 /nobreak
+echo 6
+timeout /t 1 /nobreak
+echo 5
+timeout /t 1 /nobreak
+echo 4
+timeout /t 1 /nobreak
+echo 3
+timeout /t 1 /nobreak
+echo 2
+timeout /t 1 /nobreak
+echo 1
+timeout /t 1 /nobreak
+shutdown /s /t 0
